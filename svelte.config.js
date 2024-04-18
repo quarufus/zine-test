776158537e69026f77e549f1767ab53f5074d0bb
quarufus <timonakira@gmail.com>
2024-04-18 16:18:44 +0300

    Refactor 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { preprocessMeltUI, sequence } from '@melt-ui/pp'
-import { preprocess } from 'svelte/compiler';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +15,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		//target: '#svelte',
+		adapter: node()
 	}
 };
 
