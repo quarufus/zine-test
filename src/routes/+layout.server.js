de8f7c977d00889123fc5ae53ab858179fbadbb9
quarufus <timonakira@gmail.com>
2024-04-19 18:12:34 +0300

    Home redirect

--- src/routes/+layout.server.js
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-/** @type {import('./$types').LayoutServerLoad} */
-export function load({ url }) {
-  if (url.pathname == '/') {
-	  redirect(300, '/home');
-	}
-}
