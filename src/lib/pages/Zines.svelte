03fa34013cfc42690ca3612883c4c7f54c6bc9de
quarufus <timonakira@gmail.com>
2024-04-19 19:40:09 +0300

    Correct grid

--- src/lib/pages/Zines.svelte
+++ src/lib/pages/Zines.svelte
@@ -1,27 +1,29 @@
 <script>
-    let zines = ['One', 'Two', 'Three', 'Four', 'Five'];
+    let zines = ["One", "Two", "Three", "Four", "Five"];
 </script>
 
 <div class="p-[5px]">
-<h1 class="p-[5px]">Zines</h1>
-<div class="flex flex-wrap flex-row justify-start">
-    {#each zines as zine}
-        <button
-            class="m-2.5 p-2.5 h-68 border-2-solid-black cursor-pointer"
-            on:click={() => {
-                location.href = '/zines/' + zine;
-            }}
-        >
-        {zine}
-        </button>
-    {/each}
-</div>
+    <h1 class="p-[5px]">Zines</h1>
+    <div
+        class="grid grid-cols-2 sm:grid-cols-3 min-[900px]:grid-cols-2 xl:grid-cols-3"
+    >
+        {#each zines as zine}
+            <button
+                class="m-2.5 p-2.5 h-68 border-2-solid-black cursor-pointer"
+                on:click={() => {
+                    location.href = "/zines/" + zine;
+                }}
+            >
+                {zine}
+            </button>
+        {/each}
+    </div>
 </div>
 
 <style>
     button {
         border: 2px solid black;
-        width: calc(33.3% - 44px);
         height: 17rem;
     }
 </style>
+
