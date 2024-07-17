e0d9834e505f104ce9e11e6bf7615db4d7f8524c
quarufus <timonakira@gmail.com>
2024-07-17 20:23:57 +0300

    contact

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,30 +1,29 @@
 <script>
-	import Pages from "$lib/Pages.svelte";
-	import SinglePage from "$lib/SinglePage.svelte";
-	import Cover from "$lib/pages/Cover.svelte";
-	import Home from "$lib/pages/Home.svelte";
-	import { setIndex } from "$lib/stores";
+  import Pages from "$lib/Pages.svelte";
+  import SinglePage from "$lib/SinglePage.svelte";
+  import Home from "$lib/pages/Home.svelte";
+  import { setIndex } from "$lib/stores";
 
-	setIndex(0);
-	$: innerWidth = 0;
+  setIndex(0);
+  $: innerWidth = 0;
 </script>
 
 <svelte:window bind:innerWidth />
 
 <svelte:head>
-	<meta
-		name="description"
-		content="Home page for To Bitoni, a webzine focused on art"
-	/>
+  <meta
+    name="description"
+    content="Home page for To Bitoni, a webzine focused on art"
+  />
 </svelte:head>
 
 {#if innerWidth > 900}
-	<Pages>
-		<div slot="left" />
-		<Home slot="right" />
-	</Pages>
+  <Pages>
+    <div slot="left" />
+    <Home slot="right" />
+  </Pages>
 {:else}
-	<SinglePage>
-		<Home slot="content" />
-	</SinglePage>
+  <SinglePage>
+    <Home slot="content" />
+  </SinglePage>
 {/if}
